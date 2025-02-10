88b4c08f760d6e926056bbeadad2a3adf81be53d
serrano-grilo-matias <matias.grilo@gmail.com>
2025-02-10 20:59:12 +0100

    update

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,23 @@
 package app;
 
-public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+import models.Personne;
 
+public class Application {
+
+        public static void main(String[] args) {
+                Personne p1 = new Personne("Paulin", "Udriot");
+                Personne p2 = new Personne("Loïc", "Rotzetter");
+                Personne p3 = new Personne("Ksawier", "Paskowski");
+
+                System.out.println(p1);
+                System.out.println(p2);
+                System.out.println(p3);
+
+                p1.ajouterCopain(p2);
+                p1.ajouterCopain(p3);
+
+                System.out.println("ajout de copains....");
+
+                System.out.println(p1);
+        }
 }
--- src/models/Personne.java
+++ src/models/Personne.java
@@ -1,17 +1,44 @@
 package models;
 
 public class Personne {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+    private final String prenom;
+    private final String nom;
+    public static final int MAX_COPAINS = 10;
+    private Personne[] copains;
+    private int nbCopains;
 
+    public Personne(String prenom, String nom) {
+        this.prenom = prenom;
+        this.nom = nom.toUpperCase();
+        this.copains = new Personne[MAX_COPAINS];
+        this.nbCopains = 0;
+    }
+
+    public boolean ajouterCopain(Personne copain) {
+        if (nbCopains < MAX_COPAINS) {
+            copains[nbCopains++] = copain;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean supprimerCopain(Personne copain) {
+        for (int i = 0; i < nbCopains; i++) {
+            if (copains[i] == copain) {
+                copains[i] = copains[--nbCopains];
+                copains[nbCopains] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String toString() {
+        String resultat = prenom + " " + nom + " et ses copains (";
+        for (int i = 0; i < nbCopains; i++) {
+            resultat += copains[i].prenom + " " + copains[i].nom;
+            if (i < nbCopains - 1) resultat += ", ";
+        }
+        return resultat + ")";
+    }
 }
